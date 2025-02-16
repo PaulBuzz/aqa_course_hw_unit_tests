80618421a1c1eb7b9e09a1080c7592cb57728617
Pavel <northdy12@gmail.com>
2025-02-16 13:58:26 +0100

    adds hw-4 tasks 1, 2, 3

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -14,3 +14,44 @@
   5. Проверить задание со следующими значениями в переменной age: 10, 17, 18, 19, 59, 60, 61
 
   */
+
+let minAge = 18
+let maxAge = 60
+
+function checkAge(age) {
+  if (typeof age !== "number") {
+    console.log("Incorrect data type")
+  } else if (age < minAge) {
+    console.log("You don't have access cause your age is " + age + " It's less then " + minAge)
+  } else if (age >= minAge && age < maxAge) {
+    console.log("Welcome!")
+  } else if (age > maxAge) {
+    console.log("Keep calm and look Culture channel")
+  } else {
+    console.log("Technical work")
+  }
+}
+
+console.log("Проверка значения 10:")
+checkAge(10)
+
+console.log("\nПроверка значения 17:")
+checkAge(17)
+
+console.log("\nПроверка значения 18:")
+checkAge(18)
+
+console.log("\nПроверка значения 19:")
+checkAge(19)
+
+console.log("\nПроверка значения 59:")
+checkAge(59)
+
+console.log("\nПроверка значения 60:")
+checkAge(60)
+
+console.log("\nПроверка значения 61:")
+checkAge(61)
+
+console.log("\nПроверка неправильного типа данных:")
+checkAge("18")
