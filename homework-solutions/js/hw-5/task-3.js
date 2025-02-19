a4164078100da9ef431405796f46ab19e765c0ca
Pavel <northdy12@gmail.com>
2025-02-19 22:05:52 +0100

    adds hw-5, task 3

--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -3,8 +3,21 @@
  * Переменная `word` уже создана и содержит строку со словом.
  * Ожидаемый результат для `hello`: "hello contains 2 vowels and 3 consonants".
  */
-const word = 'hello';
+const word = 'hello'
 
-let vowelsAndConsonantsResult = '';
+let vowelsAndConsonantsResult = ''
+let vowels = 0
+let consonants = 0
+const vowelsList = ['a', 'e', 'i', 'o', 'u']
 
-export { vowelsAndConsonantsResult };
+for (let i = 0; i < word.length; i++) {
+    if (vowelsList.includes(word[i])) {
+        vowels++;
+    } else {
+        consonants++
+    }
+}
+
+vowelsAndConsonantsResult = `${word} contains ${vowels} vowels and ${consonants} consonants`
+
+export { vowelsAndConsonantsResult }
