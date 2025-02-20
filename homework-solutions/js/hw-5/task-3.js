6ee86aca508836477a80a5b061926dfca8a2620c
Pavel <northdy12@gmail.com>
2025-02-20 13:23:41 +0100

    fixes task-3 to deal with hyphened words

--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -4,14 +4,15 @@
  * Ожидаемый результат для `hello`: "hello contains 2 vowels and 3 consonants".
  */
 const word = 'hello'
+const vowelsList = ['a', 'e', 'i', 'o', 'u', 'y']
+const cleanWord = word.replace(/-/g, '')
 
 let vowelsAndConsonantsResult = ''
 let vowels = 0
 let consonants = 0
-const vowelsList = ['a', 'e', 'i', 'o', 'u']
 
-for (let i = 0; i < word.length; i++) {
-    if (vowelsList.includes(word[i])) {
+for (let i = 0; i < cleanWord.length; i++) {
+    if (vowelsList.includes(cleanWord[i])) {
         vowels++;
     } else {
         consonants++
